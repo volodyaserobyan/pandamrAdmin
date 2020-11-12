bfa1bbb4a494766492c8cad11af23be0529e1e7c
aleksanyan.tigran <aleksanyan.tigran@betconstruct.com>
2020-11-12 15:19:37 +0400

    Create meeting popup

--- src/components/Modal/Success/Success.js
+++ src/components/Modal/Success/Success.js
@@ -4,14 +4,25 @@ import "./Success.scss";
 
 const Success = (props) => {
   return (
-    <div className="status-b-holder status-hidden">
-      <div className="success-cont">
-        <span className="success-cont-close"></span>
+    <div className="status-b-holder status-hidden0">
+      <div className="success-cont" style={{display: "none"}}>
+        <div className="success-cont-close"></div>
         <div className="success-cont-img">
           <img src={successImg} alt="Successfully reset" />
         </div>
         <p className="success-cont-text">{props.value}</p>
       </div>
+        <div className="meeting-cont">
+          <div className="meeting-cont-close">
+          </div>
+            <div className="meeting-cont-txt">
+                <p>you are about to delete the room<br/><span>are you sure?</span></p>
+            </div>
+            <div className="meeting-buttons">
+                <button className="meeting-btn meeting-btn-delete">Confirm</button>
+                <button className="meeting-btn meeting-btn-cancel">Cancel</button>
+            </div>
+         </div>
     </div>
   );
 };
